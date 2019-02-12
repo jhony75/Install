9f8c4a31c9e0167e20232757a9a900c51abedd5e
Jônatas Lima de Medeiros <41880573+jhony75@users.noreply.github.com>
2019-02-12 02:10:25 -0200

    Add files via upload

---
+++ RVM.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Installing gcc"
+
+pacman -S gcc --noconfirm
+
+echo "Installing rvm"
+
+gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+\curl -sSL https://get.rvm.io | bash -s stable --rails
+
+clear
+
+chmod +x installer.rb
+
+ruby installer.rb
---
+++ installer.rb
@@ -0,0 +1,25 @@
+puts "Initializing Ruby Installer"
+
+system ("clear")
+system ("pacman -Syu")
+system ("clear")
+
+programs = [
+"zathura",
+"zathura-cb",
+"zathura-djvu",
+"zathura-mupdf",
+"zathura-poppler",
+"zathura-ps",
+"texlive-most",
+"texlive-lang",
+"i3blocks",
+"vim",
+"adobe-source-code-pro-fonts"
+]
+
+programs.each { |name|
+system ("pacman -Sq #{name} --noconfirm")
+}
+
+puts "Everything Installed!"
