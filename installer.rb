01a34748f08cd9d2467623dedd05327a1675f53c
Jônatas Lima de Medeiros <41880573+jhony75@users.noreply.github.com>
2019-02-13 15:46:20 -0200

    Update installer.rb
    Added Yay (AUR helper) and fixed an issue with Zathura PDF

--- installer.rb
+++ installer.rb
@@ -9,17 +9,28 @@ programs = [
 "zathura-cb",
 "zathura-djvu",
 "zathura-pdf-mupdf",
-"zathura-pdf-poppler",
+#"zathura-pdf-poppler", Only one PDF is allowed.
 "zathura-ps",
 "texlive-most",
 "texlive-lang",
+"texmaker"
 "i3blocks",
 "vim",
+"yay",
 "adobe-source-code-pro-fonts"
+]
+
+aur = [
+  "biber",
+  "visual-studio-code-bin"
 ]
 
 programs.each { |name|
 system ("pacman -Sq #{name} --noconfirm")
+}
+
+aur.each { |name|
+system ("yay -Sq #{name} --noconfirm")
 }
 
 puts "Everything Installed!"
